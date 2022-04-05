980c69e9408cadba2d405563bcd2e8bdd4886824
Tsara Sudrajat <tsara.tf@gmail.com>
2022-04-05 14:38:07 +0700

    Restart project

--- projects/my-goodreads/src/components/shared/Button/index.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Button from './index';
-
-export default { title: 'Button' };
-
-export const primary = () => <Button kind="primary">Button</Button>;
-export const warning = () => <Button kind="warning">Button</Button>;
-export const secondary = () => <Button kind="secondary">Button</Button>;
--- projects/my-goodreads/src/components/shared/InputFile/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function InputFile(props) {
-  return (
-    <input className="file-input" type="file" name={props.name} {...props} />
-  );
-}
-
-export default InputFile;
