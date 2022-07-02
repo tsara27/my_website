0353e711332983fe6b74c27c14c7087cb9c7cc25
Tsara Fatma Larasati Sudrajat <tsara.tf@gmail.com>
2022-07-02 14:05:11 +0700

    WIP

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
-const path = require('path')
+const path = require("path");
 
 const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  }
-}
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  webpack: function (config) {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: "raw-loader",
+    });
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
